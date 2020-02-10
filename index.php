32284d6377d5bf1d6b251929655f8d77a6077741
Wahyu Cahyadi <wahyuc76@yahoo.com>
2020-02-10 16:04:52 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 </head>
 <h1>hello world</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nemo vitae sunt vero facere eius eveniet qui asperiores hic magni odio error tempora dolore deserunt harum aperiam cum unde totam aut voluptatem, provident alias exercitationem similique. Maxime, beatae consectetur, maiores similique quo cumque sit, ea vel quibusdam incidunt quis molestias. Dicta eligendi earum provident, dignissimos vel accusamus a fuga tempore ducimus minima ipsa, minus, id corporis. Pariatur id, tempora, suscipit, voluptate provident repellendus deserunt ut officiis soluta ratione reiciendis debitis facere. Sapiente saepe alias nihil officiis exercitationem et hic quod iusto deserunt quis. Aut officiis aperiam iure totam perferendis fugiat?</p>
+<li>
+    <ul>satu</ul>
+    <ul>dua</ul>
+</li>
 
 <body>
 
